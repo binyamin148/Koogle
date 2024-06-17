dcdc0c5af5615398cbf6a648bf0f825b07a21f53
binyamin148 <benibur2079@gmail.com>
2024-06-17 14:26:02 +0300

    download html

---
+++ Crawler/inc/download.hpp
@@ -0,0 +1,16 @@
+#ifndef DOWNLOAD_HPP
+#define DOWNLOAD_HPP
+
+#include <string>
+#include <vector>
+
+class Download
+{
+public:
+    Download() = default;
+    ~Download() = default;
+
+    static std::string download_html(const std::string& url);
+};
+
+#endif // DOWNLOAD_HPP
